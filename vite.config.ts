27a7e58d723baf7541cbd1567a381439ba883c7c
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-01-31 20:49:39 +0100

    Added node resolve for rollup and using vitest/config defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [dts({ rollupTypes: true })],
@@ -12,6 +12,15 @@ export default defineConfig({
       formats: ['es', 'cjs'],
     },
     sourcemap: true,
+  },
+  resolve: {
+    /**
+     * Add resolution behavior for Node.js to rollup
+     *
+     * @see https://github.com/rollup/plugins/tree/master/packages/node-resolve#exportconditions
+     * @see https://github.com/uuidjs/uuid/issues/544
+     */
+    conditions: ['node'],
   },
   test: {
     include: ['test/**/*.test.ts'],
