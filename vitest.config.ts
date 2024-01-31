da0ba60e10c21aab398b9ef17924bde1803361e8
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-01-31 23:14:45 +0100

    Exclude dist files

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['test/util.test.ts'],
+    exclude: ['dist/**/*'],
     coverage: {
       reporter: ['text', 'lcov'],
     },
