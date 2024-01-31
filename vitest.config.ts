743c1654447437e6a169a42d0e66afda2ff5c3d6
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-01-31 23:16:47 +0100

    Only in coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['dist/**/*'],
     coverage: {
       reporter: ['text', 'lcov'],
+      exclude: ['dist/**/*'],
     },
   },
 });
